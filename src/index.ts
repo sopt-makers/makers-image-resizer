0047ba63eccc5e1db2692af9548dbd99f0bcc15e
borimong <kimhyunsu11@naver.com>
2024-09-29 14:26:24 +0900

    initial: set up initial image resizer

--- src/index.ts
+++ src/index.ts
@@ -11,11 +11,59 @@
  * Learn more at https://developers.cloudflare.com/workers/
  */
 
+interface CfImageOptions {
+	width?: number;
+	format?: 'avif' | 'webp';
+}
+
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
-		const url = new URL(request.url);
-		const query = url.searchParams.get("ab")
+		// const url = new URL(request.url);
 
-		return new Response('Hello World! ' + url.pathname + query);
+		// const query = url.searchParams.get('ab');
+
+		// return new Response('Hello World! ' + url.pathname + query);
+
+		let url = new URL(request.url);
+
+		let options: { cf: { image: CfImageOptions } } = { cf: { image: {} } };
+
+		const widthParam = url.searchParams.get('width');
+		if (widthParam) {
+			const width = parseInt(widthParam, 10);
+			if (!isNaN(width)) {
+				options.cf.image.width = width;
+			}
+		}
+
+		const accept = request.headers.get('Accept') ?? '';
+		if (/image\/avif/.test(accept)) {
+			options.cf.image.format = 'avif';
+		} else if (/image\/webp/.test(accept)) {
+			options.cf.image.format = 'webp';
+		}
+
+		const imageURL = url.searchParams.get('image');
+		if (!imageURL) return new Response('Missing "image" value', { status: 400 });
+
+		try {
+			const { hostname, pathname } = new URL(imageURL);
+
+			if (!/\.(jpe?g|png|gif|webp)$/i.test(pathname)) {
+				return new Response('Disallowed file extension', { status: 400 });
+			}
+
+			if (hostname !== 'makers-web-img.s3.ap-northeast-2.amazonaws.com') {
+				return new Response('Must use "example.com" source images', { status: 403 });
+			}
+		} catch (err) {
+			return new Response('Invalid "image" value', { status: 400 });
+		}
+
+		const imageRequest = new Request(imageURL, {
+			headers: request.headers,
+		});
+
+		return fetch(imageRequest, options);
 	},
 } satisfies ExportedHandler<Env>;
